6e006f9d70ad491bdabc8b7033f5b73bd388bc0c
samuelj123 <samuelj123@gmail.com>
2023-06-20 18:26:26 +1000

    Json Content capable Able to upload and download Json content and able to use typescript with no problem

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let rawdata="";
+	let dashboard=false;
+	let nope:boolean;
+	let data:Data;
+	let string: string;
+
+	interface Data{
+		accounts: object[];
+		records: object[];
+		budgets: object[];
+	}
+
+	const submitdata = () => {
+		nope = true;
+		data = JSON.parse(rawdata);
+		nope = false;
+		dashboard = true;
+		string = "data: text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+	}
+
+	const reset = () => {
+		rawdata = "";
+		dashboard = false;
+		nope = false;
+		string = "";
+	}
+</script>
+
+{#if !dashboard}
+	<input
+		type="text"
+		bind:value={rawdata}
+	/>
+	<button on:click={submitdata}>Submit</button>
+{:else}
+	<a href={string} download="finance.json">Download</a>
+	<button on:click={reset}>Back</button>
+{/if}
+{#if nope}
+	<h2>Nope</h2>
+	<button on:click={reset}>Back</button>
+{/if}
